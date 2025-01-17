bed72bed80737daad51545aefd579623b85d4c2b
ASHISH KASHYAP <ashish.k22012005@gmail.com>
2025-01-17 12:55:42 +0530

    Initial Upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/OchiDesign/",
   plugins: [react()],
-  base:"/vite-react-deploy"
+  
 })
